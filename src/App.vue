5d38b124d442576420ce8b923dbaab6642477967
peter <petervanrijn@hhotmail.nl>
2023-04-28 13:52:33 +0200

    test Api endpoint call function

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <router-view />
+  <!-- <router-view /> -->
+  {{ message }}
 </template>
 
 <script setup>
-  //
+  import axios from 'axios'
+  import { onMounted, ref } from 'vue';
+
+  let message = ref([])
+
+  axios.defaults.baseURL = 'http://127.0.0.1:80/api/';
+  onMounted(() => {
+    axios.get('/test').then((res) => {
+      console.log(res)
+      message.value = ""
+    })
+    .catch((error) => {
+      console.log(error.res);
+    });
+  });
 </script>
