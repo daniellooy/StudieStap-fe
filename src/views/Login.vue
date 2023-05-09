e1e4c68624c3eac81a8cd9078db08eb62143337f
Peter van Rijn <43537276+petervanrijn@users.noreply.github.com>
2023-05-09 20:33:11 +0200

    login send the user back

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,7 +14,7 @@ import {useAppStore} from "@/store/store";
 
 const store = useAppStore()
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'http://localhost:8000',
   withCredentials: true,
   headers: {
     "accept": 'application/json',
