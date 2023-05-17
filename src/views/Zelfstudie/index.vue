3a900b9ee0edb826f92278227958ad243f30f949
Joey Peschier <62258312+joeyyeoj@users.noreply.github.com>
2023-05-17 09:02:46 +0200

    stuff

---
+++ src/views/Zelfstudie/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <module_list :title="'Uitgelicht'" :videos="videos"/>
+</template>
+
+<script setup>
+import Module_list from "@/components/zelfstudie/module_list.vue";
+
+
+const videos = [
+  {
+    title: 'Video 1',
+    slug: 'video1'
+  },
+  {
+    title: 'Video 2',
+    slug: 'video1'
+  },
+  {
+    title: 'Video 3',
+    slug: 'video1'
+  }
+]
+
+</script>
+
+<style scoped>
+
+</style>
