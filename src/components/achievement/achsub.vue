a3f1703e9fe0ff954beee6989a28968568f9fe0c
daniellooy <daniel.looijestijn@hotmail.com>
2023-06-04 12:42:22 +0200

    achievement page styling

--- src/components/achievement/achsub.vue
+++ src/components/achievement/achsub.vue
@@ -1,24 +1,18 @@
 <template>
   <!-- sub zit al in dubdone => aangevraagd, gedaan of denied-->
-    <section class="achsub" v-if="checkIfDone() == 'accepted'">
+    <section class="achsub" v-if="status == 'accepted'">
       <section class="achsub__inside">
-        <section class="achsub__title">
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
-        </section>
+          <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
       </section>
     </section>
-    <section class="achsub" v-else-if="checkIfDone() == 'denied'">
-      <section class="achsub__inside">
-        <section class="achsub__title">
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>
-        </section>
+    <section class="achsub" v-else-if="status == 'denied'">
+      <section class="achsub__inside__denied achsub__inside">
+          <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"/></svg>
       </section>
     </section>
-    <section class="achsub" v-else-if="checkIfDone() == 'pending'">
-      <section class="achsub__inside">
-        <section class="achsub__title">
-          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 32C0 14.3 14.3 0 32 0H64 320h32c17.7 0 32 14.3 32 32s-14.3 32-32 32V75c0 42.4-16.9 83.1-46.9 113.1L237.3 256l67.9 67.9c30 30 46.9 70.7 46.9 113.1v11c17.7 0 32 14.3 32 32s-14.3 32-32 32H320 64 32c-17.7 0-32-14.3-32-32s14.3-32 32-32V437c0-42.4 16.9-83.1 46.9-113.1L146.7 256 78.9 188.1C48.9 158.1 32 117.4 32 75V64C14.3 64 0 49.7 0 32zM96 64V75c0 25.5 10.1 49.9 28.1 67.9L192 210.7l67.9-67.9c18-18 28.1-42.4 28.1-67.9V64H96zm0 384H288V437c0-25.5-10.1-49.9-28.1-67.9L192 301.3l-67.9 67.9c-18 18-28.1 42.4-28.1 67.9v11z"/></svg>
-        </section>
+    <section class="achsub" v-else-if="status == 'pending'">
+      <section class="achsub__inside__pending achsub__inside">
+          <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 32C0 14.3 14.3 0 32 0H64 320h32c17.7 0 32 14.3 32 32s-14.3 32-32 32V75c0 42.4-16.9 83.1-46.9 113.1L237.3 256l67.9 67.9c30 30 46.9 70.7 46.9 113.1v11c17.7 0 32 14.3 32 32s-14.3 32-32 32H320 64 32c-17.7 0-32-14.3-32-32s14.3-32 32-32V437c0-42.4 16.9-83.1 46.9-113.1L146.7 256 78.9 188.1C48.9 158.1 32 117.4 32 75V64C14.3 64 0 49.7 0 32zM96 64V75c0 25.5 10.1 49.9 28.1 67.9L192 210.7l67.9-67.9c18-18 28.1-42.4 28.1-67.9V64H96zm0 384H288V437c0-25.5-10.1-49.9-28.1-67.9L192 301.3l-67.9 67.9c-18 18-28.1 42.4-28.1 67.9v11z"/></svg>
       </section>
     </section>
 
@@ -45,10 +39,8 @@
 
       <!-- locked -->
       <section class="achsub__locked" v-else>
-        <section class="achsub__inside__lock">
-          <section class="achsub__title">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg>
-          </section>
+        <section class="achsub__inside__lock achsub__inside">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/></svg>
         </section>
       </section>
 
@@ -69,49 +61,30 @@
     data(){
       return {
         hover: false,
+        status: "",
       }
     },
 
     mounted() {
-      console.log(this.sub);
+      if(this.sub.done.length > 0){
+          if(this.sub.done[0].status == "pending" || this.request == true){
+            this.status = "pending"
+          }
+          else if(this.sub.done[0].status == "accepted"){
+            this.status = "accepted"
+          }
+          else if(this.sub.done[0].status == "denied"){
+            this.status = "denied"
+          }
+        }
     },
 
     methods: {
-      //checken of sub in subs_done zit
-      checkIfDone(){
-        if(this.sub.done.length > 0){
-          if(this.sub.done[0].status == "pending"){
-            return "pending"
-          }
-          else if(this.sub.done[0].status == "accepted"){
-            return "accepted"
-          }
-          else if(this.sub.done[0].status == "denied"){
-            return "denied"
-          }
-        }
-        
-        else{
-          return false
-        }
-
-
-        // for(let i=0; i < this.sub.done.length; i++){
-        //   if(this.sub.done[i].sub_id == this.sub.id){
-        //     return "accepted";
-        //   }
-        // }
-        // return false;
-      },
 
       isUnlocked(){
         let highestRang = 0;
-        console.log("Test");
-        // console.log(this.sub);
-        // console.log(this.data);
-        console.log(this.data.length);
         for(let i=0; i < this.data.length; i++){
-          console.log(this.data[i]);
+          // console.log(this.data[i]);
           if(this.data[i].done.length > 0){
             if(this.data[i].rang > highestRang){
               highestRang = this.data[i].rang
@@ -136,7 +109,11 @@
       },
 
       requestAchievement(){
-        axios.post('http://localhost:8000/api/subs/' + this.sub.id + '/request');
+        axios.post('http://localhost:8000/api/subs/' + this.sub.id + '/request')
+        .then((response)=>{
+          this.status = "pending";
+          // console.log(this.request);
+        });
       }
     }
   }
@@ -180,14 +157,15 @@
   justify-content: center;
   flex-direction: column;
 }
+
+.achsub__inside__pending{
+  background: #ffec42ce;
+}
+.achsub__inside__denied{
+  background: #ff3838d2;
+}
 .achsub__inside__lock {
-  height: 4.4rem;
-  width: 4.4rem;
-  border-radius: 1rem;
   background: #858585;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 }
 
 .achsub__button{
@@ -213,6 +191,20 @@
   font-weight:500;
   font-size: large;
   color: white;
+}
+
+
+/* Accepted  */
+.achsub__icon__accepted{
+  fill: white;
+}
+
+.achsub__icon{
+  
+}
+
+svg{
+  fill: white;
 }
 
 
