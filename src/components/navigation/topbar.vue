fc35cdf2504f4d41de0ad537f0e8720f1461e56b
Joey Peschier <62258312+joeyyeoj@users.noreply.github.com>
2023-06-07 10:14:16 +0200

    Topbar styling verbetering

--- src/components/navigation/topbar.vue
+++ src/components/navigation/topbar.vue
@@ -10,18 +10,7 @@
         {{ currentRouteName }}
       </div>
     </div>
-    <div class="profile-info">
-      <span class="profile-info-notifications">
-        <div class="number">
-          1
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-        </svg>
-      </span>
-      <span class="username">{{ user ? user.first_name : 'Niet ingelogd' }}</span>
-      <div class="profile-info-avatar"></div>
-    </div>
+    <Profile />
   </div>
 </template>
 
@@ -55,6 +44,7 @@ function openSidebar(){
   font-size: 18px;
   position: fixed;
   width: 80%;
+  z-index: 5;
 }
 
 .profile-info {
